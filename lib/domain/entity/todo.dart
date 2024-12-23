650532688e955517f3058fce5844c49bdea93cb4
KodaiIkari <Ikari.Koudai@gmail.com>
2024-12-23 22:06:01 +0900

    TODOアイテムの情報を表すデータクラスの作成

---
+++ lib/domain/entity/todo.dart
@@ -0,0 +1,13 @@
+class Todo {
+  final int id;
+  final String title;
+  final DateTime deadline;
+  final String description;
+
+  Todo({
+    required this.id,
+    required this.title,
+    required this.deadline,
+    required this.description,
+  });
+}
