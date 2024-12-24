25a80e31f2e8bca195f1871f594751d50d092345
KodaiIkari <Ikari.Koudai@gmail.com>
2024-12-24 20:42:57 +0900

    トップ画面で＋ボタンを押したらリストが増えるサンプルの作成

--- lib/ui/top/top_page.dart
+++ lib/ui/top/top_page.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:training_todo/model/todo.dart';
+import 'package:training_todo/ui/top/todo_list.dart';
 
 class TopPage extends StatefulWidget {
-  const TopPage({super.key, required this.title});
+  TopPage({super.key, required this.title});
 
   final String title;
+
+  final List<Todo> items = [];
 
   @override
   State<TopPage> createState() => _TopPageState();
 }
 
 class _TopPageState extends State<TopPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,21 +23,22 @@ class _TopPageState extends State<TopPage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(child: TodoList(items: widget.items)),
+            ]),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
         tooltip: 'Increment',
+        onPressed: () {
+          setState(() {
+            widget.items.add(Todo(
+                id: widget.items.length,
+                title: "title${widget.items.length}",
+                deadline: DateTime.now(),
+                description: "description"));
+          });
+        },
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
