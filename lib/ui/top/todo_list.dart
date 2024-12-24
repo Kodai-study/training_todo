98d0928c0c69967cd08c653bb19f258f053c8866
KodaiIkari <Ikari.Koudai@gmail.com>
2024-12-24 20:39:05 +0900

    コンストラクタで受け取ったリストのListViewを表示するロジックを作成

--- lib/ui/top/todo_list.dart
+++ lib/ui/top/todo_list.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:training_todo/model/todo.dart';
 
 class TodoList extends StatelessWidget {
-  const TodoList({super.key});
+  const TodoList({super.key, required this.items});
+
+  final List<Todo> items;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index].title),
+          );
+        });
   }
 }
