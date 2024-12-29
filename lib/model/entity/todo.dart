84507d33db2e07743bc8868d56ea79c96ab2252b
KodaiIkari <Ikari.Koudai@gmail.com>
2024-12-29 23:24:31 +0900

    DBの管理クラスで、DBを作るファイルパスをコンストラクタで受け取るように変更

--- lib/model/entity/todo.dart
+++ lib/model/entity/todo.dart
@@ -1,10 +1,7 @@
 import 'dart:io';
-import 'dart:ui';
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
 
 part 'todo.g.dart';
 
@@ -20,7 +17,9 @@ class Todo extends Table {
 
 @DriftDatabase(tables: [Todo])
 class TodoDatabase extends _$TodoDatabase {
-  TodoDatabase() : super(_openConnection());
+  final File dbFile;
+
+  TodoDatabase(this.dbFile) : super(_openConnection(dbFile));
 
   Future<List<TodoData>> getTodos() {
     return select(todo).get();
@@ -30,10 +29,8 @@ class TodoDatabase extends _$TodoDatabase {
   int get schemaVersion => 1;
 }
 
-LazyDatabase _openConnection() {
+LazyDatabase _openConnection(File file) {
   return LazyDatabase(() async {
-    final dbFolder = await getApplicationDocumentsDirectory();
-    final file = File(join(dbFolder.path, 'db.sqlite'));
     return NativeDatabase(file);
   });
 }
