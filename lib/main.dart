ea820e8f4c7db3762ae34f33e3354e779e4eaf98
KodaiIkari <Ikari.Koudai@gmail.com>
2025-01-02 12:29:15 +0900

    アプリ起動時にアプリ情報のディレクトリにDBを作成してトップ画面を正しく表示させるように

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:training_todo/model/entity/todo.dart';
+import 'package:training_todo/model/todo_list_manager.dart';
 import 'package:training_todo/ui/top/top_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  Future<TodoDatabase> _createDb() async {
+    final dirPath = (await getApplicationDocumentsDirectory()).path;
+    return TodoDatabase(File(join(dirPath, 'test.db')));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +27,16 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: TopPage(),
+      home: FutureBuilder(
+          future: _createDb(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData || snapshot.hasError) {
+              return CircularProgressIndicator();
+            }
+            return TopPage(
+              manager: TodoListManager(snapshot.data!),
+            );
+          }),
     );
   }
 }
