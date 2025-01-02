b9dbe9360201eef7dbc409e2654d3095a4eb1494
KodaiIkari <Ikari.Koudai@gmail.com>
2025-01-02 12:25:49 +0900

    トップ画面で+ボタンを押したときにDBにTodoが追加されるように変更

--- lib/ui/top/top_page.dart
+++ lib/ui/top/top_page.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:training_todo/const/strings.dart';
 import 'package:training_todo/model/entity/todo.dart';
+import 'package:training_todo/model/todo_list_manager.dart';
 import 'package:training_todo/ui/top/todo_list.dart';
 
 class TopPage extends StatefulWidget {
-  TopPage({super.key});
+  final TodoListManager manager;
 
-  final List<TodoData> items = [];
+  const TopPage({super.key, required this.manager});
 
   @override
   State<TopPage> createState() => _TopPageState();
 }
 
 class _TopPageState extends State<TopPage> {
+  late List<TodoData> items;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,21 +24,34 @@ class _TopPageState extends State<TopPage> {
         title: Text(TopPageStrings.title),
       ),
       body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(child: TodoList(items: widget.items)),
-            ]),
+        child: FutureBuilder(
+            future: widget.manager.data,
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return CircularProgressIndicator();
+              }
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+              items = snapshot.data!;
+              return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(child: TodoList(items: items)),
+                  ]);
+            }),
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Increment',
-        onPressed: () {
+        onPressed: () async {
+          await widget.manager.add(TodoData(
+              id: items.length,
+              title: "title${items.length}",
+              deadline: DateTime.now(),
+              description: "description"));
+          final updatedItem = await widget.manager.data;
           setState(() {
-            widget.items.add(TodoData(
-                id: widget.items.length,
-                title: "title${widget.items.length}",
-                deadline: DateTime.now(),
-                description: "description"));
+            items = updatedItem;
           });
         },
         child: const Icon(Icons.add),
