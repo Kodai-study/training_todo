6a6d4038866df034725ebbfcbaa7af7e54c09343
KodaiIkari <Ikari.Koudai@gmail.com>
2025-02-23 21:45:31 +0900

    SCRUM-3：トップ画面を、画面の共通レイアウトを使用して作成

--- lib/ui/top/top_page.dart
+++ lib/ui/top/top_page.dart
@@ -1,49 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:training_todo/ui/component/screen_base.dart';
 
-class TopPage extends StatefulWidget {
-  const TopPage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<TopPage> createState() => _TopPageState();
-}
-
-class _TopPageState extends State<TopPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class TopPage extends StatelessWidget {
+   const TopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return ScreenBase(body: Text("トップ画面"), title: "title");
   }
 }
