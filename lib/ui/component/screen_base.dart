14dfe228cbefdff909ec10dff264122d7a9be660
KodaiIkari <Ikari.Koudai@gmail.com>
2025-02-23 21:44:37 +0900

    SCRUM-3:ヘッダに表示するタイトル文字を引数から受け取るように変更

--- lib/ui/component/screen_base.dart
+++ lib/ui/component/screen_base.dart
@@ -4,11 +4,12 @@ import 'header.dart';
 
 class ScreenBase extends StatelessWidget {
   final Widget body;
+  final String title;
 
-  const ScreenBase({super.key, required this.body});
+  const ScreenBase({super.key, required this.body, required this.title});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: Header(title: "ページ"), body: body);
+    return Scaffold(appBar: Header(title: title), body: body);
   }
 }
