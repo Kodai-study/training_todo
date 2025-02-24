db25e7ab7feab54a3fa2ccf5782f21ae6a17095f
KodaiIkari <Ikari.Koudai@gmail.com>
2025-02-24 10:51:11 +0900

    DateTimeをアプリで共通で使う形式にフォーマットする拡張関数を作成

--- lib/extension/datetime_extension.dart
+++ lib/extension/datetime_extension.dart
@@ -1,6 +1,13 @@
+import 'package:intl/intl.dart';
+
 extension DatetimeExtension on DateTime {
   DateTime removeSubSecond() {
     return subtract(
         Duration(milliseconds: millisecond, microseconds: microsecond));
+  }
+
+  String formatAsYMD() {
+    final dateFormat = DateFormat('yyyy/MM/dd');
+    return dateFormat.format(this);
   }
 }
