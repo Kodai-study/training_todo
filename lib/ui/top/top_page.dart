a6edd0b8b92c7e5b03fb6ce90d9d7d73ce79f6c6
KodaiIkari <Ikari.Koudai@gmail.com>
2025-02-24 19:27:12 +0900

    SCRUM-3:トップ画面の内部にTodoリストを表示させるように変更

--- lib/ui/top/top_page.dart
+++ lib/ui/top/top_page.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:training_todo/ui/component/screen_base.dart';
+import 'package:training_todo/ui/top/todo_list.dart';
+import 'package:training_todo/ui/top/todo_list_provider.dart';
 
 class TopPage extends StatelessWidget {
-   const TopPage({super.key});
+  const TopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ScreenBase(body: Text("トップ画面"), title: "title");
+    return ScreenBase(
+      title: "トップ画面",
+      body: ChangeNotifierProvider(
+          create: (_) => TodoListProvider(), child: TodoList()),
+    );
   }
 }
