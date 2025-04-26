0562b8afc07f7eb5d3d1e9bcddb71342a5b36c30
KodaiIkari <Ikari.Koudai@gmail.com>
2025-04-26 14:32:13 +0900

    Todoリストで、表示するリストが完了済みかどうかをboolで受け取るように変更

--- lib/ui/top/todo_list.dart
+++ lib/ui/top/todo_list.dart
@@ -4,18 +4,21 @@ import 'package:training_todo/ui/top/todo_list_tile.dart';
 import 'package:training_todo/ui/top/view_models/top_viewmodel.dart';
 
 class TodoList extends StatelessWidget {
-  const TodoList({super.key});
+  const TodoList({super.key, required this.isCompleted});
+
+  final bool isCompleted;
 
   @override
   Widget build(BuildContext context) {
     return Consumer<TopViewmodel>(
       builder: (_, provider, child) {
-        return ListView.builder(
-          itemBuilder: (_, i) => Column(
-            children: [TodoListTile(todoData: provider.todoList[i]), Divider()],
-          ),
-          itemCount: provider.todoList.length,
-        );
+        final list =
+            isCompleted ? provider.completedList : provider.unCompletedList;
+
+        return ListView.separated(
+            itemBuilder: (_, i) => TodoListTile(todoData: list[i]),
+            itemCount: list.length,
+            separatorBuilder: (context, index) => Divider());
       },
     );
   }
--- lib/ui/top/top_screen.dart
+++ lib/ui/top/top_screen.dart
@@ -3,22 +3,28 @@ import 'package:provider/provider.dart';
 import 'package:training_todo/ui/top/todo_list.dart';
 import 'package:training_todo/ui/top/view_models/top_viewmodel.dart';
 
-import '../../model/todo_item.dart';
-
 class TopScreen extends StatelessWidget {
   const TopScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      context
-          .read<TopViewmodel>()
-          .addTodo
-          .execute(TodoItem(1, "title", DateTime.now(), false));
-    });
-    return Scaffold(
-      appBar: AppBar(title: Text("data")),
-      body: TodoList(),
+    context.read<TopViewmodel>().loadList.execute();
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("data"),
+          bottom: TabBar(tabs: <Widget>[
+            Tab(text: "未完了"),
+            Tab(text: "完了済み"),
+          ]),
+        ),
+        body: TabBarView(children: <Widget>[
+          TodoList(isCompleted: false),
+          TodoList(isCompleted: true)
+        ]),
+      ),
     );
   }
 }
