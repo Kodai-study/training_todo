69e90401cde7651a211541addd21da31624ec331
KodaiIkari <Ikari.Koudai@gmail.com>
2025-05-02 20:46:34 +0900

    テスト用のTodoリストを作成するメソッドの定義

--- testing/models/todo.dart
+++ testing/models/todo.dart
@@ -13,6 +13,24 @@ final kTodoItemCompleted = TodoItem(0, '', DateTime(2025, 1, 1), true,
     deadline: DateTime(2025, 3, 1),
     completion: DateTime(2025, 2, 1));
 
+List<TodoItem> createDefaultUncompletedTodoList(int size, {int offset = 0}) {
+  final todoItemList = <TodoItem>[];
+  for (int i = 0; i < size; i++) {
+    final id = i + offset + 1;
+    todoItemList.add(kTodoItem.copyWith(id: id, title: "title$id"));
+  }
+  return todoItemList;
+}
+
+List<TodoItem> createDefaultCompletedTodoList(int size, {int offset = 0}) {
+  final todoItemList = <TodoItem>[];
+  for (int i = 0; i < size; i++) {
+    final id = i + offset + 1;
+    todoItemList.add(kTodoItemCompleted.copyWith(id: id, title: "title$id"));
+  }
+  return todoItemList;
+}
+
 final kTodoCompanion = TodoCompanion.insert(
     title: "title",
     createdAt: DateTime(2025, 1, 1),
