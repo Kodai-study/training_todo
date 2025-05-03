f8487331b269fdcff93f6e70ac061e68890b4ba3
KodaiIkari <Ikari.Koudai@gmail.com>
2025-05-03 17:43:35 +0900

    認証のプロバイダを作成

---
+++ lib/repositories/auth/auth_repository.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../model/user/user.dart';
+import '../../util/result.dart';
+
+abstract class AuthRepository extends ChangeNotifier {
+  bool get isAuthenticated;
+
+  User? get loggedInUser;
+
+  /// Get current user
+  Future<Result<User>> login({required String email, required String password});
+
+  /// Sign out
+  Future<Result<void>> signOut();
+}
