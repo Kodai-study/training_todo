8cc2adc1431e2ed615e8295a2878544ec1faa7f0
KodaiIkari <Ikari.Koudai@gmail.com>
2025-05-15 15:43:25 +0900

    タスクの追加画面のViewModelを実装

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -5,23 +5,19 @@ import 'package:training_todo/ui/add_todo/view_models/add_todo_view_model.dart';
 import 'package:training_todo/ui/top/top_screen.dart';
 
 import '../ui/add_todo/add_todo_screen.dart';
-import '../ui/top/view_models/top_viewmodel.dart';
 
 GoRouter router() => GoRouter(initialLocation: Routes.home, routes: [
       GoRoute(
         path: Routes.home,
         builder: (context, state) {
-          return ChangeNotifierProvider(
-            create: (context) => TopViewmodel(context.read()),
-            child: TopScreen(),
-          );
+          return TopScreen();
         },
       ),
       GoRoute(
         path: Routes.addTodo,
         builder: (context, state) {
           return ChangeNotifierProvider(
-            create: (context) => AddTodoViewModel(),
+            create: (context) => AddTodoViewModel(context.read()),
             child: AddTodoScreen(),
           );
         },
--- lib/ui/add_todo/view_models/add_todo_view_model.dart
+++ lib/ui/add_todo/view_models/add_todo_view_model.dart
@@ -1,3 +1,39 @@
 import 'package:flutter/cupertino.dart';
+import 'package:training_todo/model/todo_item.dart';
+import 'package:training_todo/ui/top/view_models/top_viewmodel.dart';
+import 'package:training_todo/util/command.dart';
+import 'package:training_todo/util/result.dart';
 
-class AddTodoViewModel extends ChangeNotifier {}
+class AddTodoViewModel extends ChangeNotifier {
+  final TopViewmodel _topViewmodel;
+  late Command1<void, TodoItem> createNewTask;
+
+  AddTodoViewModel(this._topViewmodel) {
+    createNewTask = Command1(_createNewTask);
+  }
+
+  Future<Result<void>> _createNewTask(TodoItem item) async {
+    await _topViewmodel.addTodo.execute(item);
+    if (_topViewmodel.addTodo.error) {
+      return Result.error((_topViewmodel.addTodo.result as Error).error);
+    }
+    return Result.ok(null);
+  }
+
+  TodoItem createTodoItem(
+    String title,
+    String description,
+    String deadlineStr,
+  ) {
+    return TodoItem(
+      0,
+      title,
+      DateTime.now(),
+      false,
+      description: description.isEmpty ? null : description,
+      deadline: deadlineStr.isEmpty
+          ? null
+          : DateTime.parse(deadlineStr.replaceAll("/", "-")),
+    );
+  }
+}
