37e5d4c679336511dd7cc8febbdecac602808869
KodaiIkari <Ikari.Koudai@gmail.com>
2025-07-06 17:31:03 +0900

    統合テストに必要な依存関係を追加

--- testing/app.dart
+++ testing/app.dart
@@ -2,10 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:drift/native.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+import 'package:training_todo/main.dart';
 import 'package:training_todo/model/todo_item.dart';
+import 'package:training_todo/repositories/auth/auth_repository.dart';
+import 'package:training_todo/repositories/auth/auth_repository_demo.dart';
+import 'package:training_todo/repositories/todo/todo_repository.dart';
+import 'package:training_todo/repositories/todo/todo_repository_database.dart';
+import 'package:training_todo/services/database/app_database.dart';
+import 'package:training_todo/ui/top/view_models/top_viewmodel.dart';
 import 'package:training_todo/util/result.dart';
 
 import 'models/todo.dart';
@@ -26,4 +35,17 @@ Future<void> testApp(WidgetTester tester, Widget body) async {
       ),
     ),
   );
+}
+
+Future<void> startIntegrationTest(WidgetTester tester) async {
+  await tester.pumpWidget(MultiProvider(providers: [
+    Provider<AppDatabase>(
+        create: (context) => AppDatabase(NativeDatabase.memory())),
+    Provider<TodoRepository>(
+      create: (context) => TodoRepositoryDatabase(context.read()),
+    ),
+    ChangeNotifierProvider<AuthRepository>(
+        create: (context) => AuthRepositoryDemo()),
+    ChangeNotifierProvider(create: (context) => TopViewmodel(context.read())),
+  ], child: MyApp()));
 }
